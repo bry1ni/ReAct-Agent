321e4cd63720898c5c29f65ef6e435dae81b7323
Ryan <r.benatallah01@gmail.com>
2025-01-31 18:24:59 +0100

    (notebook): main.ipynb for agent graph

--- src/tasks/agents/react.py
+++ src/tasks/agents/react.py
@@ -1,7 +1,10 @@
 from src.tasks.agents import workflow, memory
+from IPython.display import Image
 
 app = workflow.compile(
     checkpointer=memory,
     interrupt_before=[],  # Add node names here to update state before they're called
     interrupt_after=[],  # Add node names here to update state after they're called
     )
+
+agent_graph = Image(app.get_graph().draw_mermaid_png())
